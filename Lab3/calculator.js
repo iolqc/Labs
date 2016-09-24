f24b3208c876c2bdd26cb40a0553cf291e9ca737
Safa89 <safa.houimli@yahoo.com>
2016-09-24 16:37:46 -0400

    Opérations de base et trigonométriques

---
+++ Lab3/calculator.js
@@ -0,0 +1,97 @@
+
+var Calculator = function() {};
+
+function isNumber(obj) { return (!isNaN(parseFloat(obj)))}
+
+var result = 0;
+
+Calculator.prototype.getResult = function() {
+    console.log("Result = " + result);
+};
+
+Calculator.prototype.add = function(number) {
+    if(isNumber(number))
+    {
+        result = result + number;
+        return this;
+    }
+    else
+    {
+        throw new Error(number + " is not a number");
+    }
+};
+
+Calculator.prototype.subtract = function(number) {
+    if(isNumber(number)) {
+        result = result - number;
+        return this;
+    }
+    else
+    {
+        throw new Error(number + " is not a number");
+    }
+};
+
+Calculator.prototype.divide = function(number) {
+    if(isNumber(number) && number != 0) {
+        result = result / number;
+        return this;
+    }
+    else
+    {
+        if(!isNumber(number)) {
+            throw new Error(number + " is not a number");
+        }
+        else if(number == 0) {
+            throw new Error("unable to divide by 0");
+        }
+    }
+};
+
+Calculator.prototype.multiply = function(number) {
+    if(isNumber(number)) {
+        result = result * number;
+        return this;
+    }
+    else
+    {
+        throw new Error(number + " is not a number");
+    }
+};
+
+Calculator.prototype.cos = function(number) {
+    if(isNumber(number)) {
+        result = Math.cos(number);
+        return this;
+    }
+    else
+    {
+        throw new Error(number + " is not a number");
+    }
+};
+
+Calculator.prototype.sin = function(number) {
+    if(isNumber(number)) {
+        result = Math.sin(number);
+        return this;
+    }
+    else
+    {
+        throw new Error(number + " is not a number");
+    }
+};
+
+Calculator.prototype.tan = function(number) {
+    if(isNumber(number)) {
+        result = Math.tan(number);
+        return this;
+    }
+    else
+    {
+        throw new Error("Error: " + number + " is not a number");
+    }
+};
+
+// test
+var calcul = new Calculator();
+calcul.add(2).divide(4).multiply(5).subtract(3).cos(45).getResult();
